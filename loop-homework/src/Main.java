a95513f2fe276a38cf29d6f6739af90f021126c4
lukakharkhelauri <148798486+lukakharkhelauri@users.noreply.github.com>
2024-10-23 19:25:35 +0400

    Add files via upload

---
+++ loop-homework/src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        //exercise 1
+//        int sum = 0;
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 5 == 0) {
+//                sum += i;
+//            }
+//        }
+//        System.out.println(sum);
+
+        //exercise 2
+//        int number = 123456;
+//        String numStr = String.valueOf(number);
+//        System.out.println(numStr.length());
+        // loopit ver gavakete
+
+
+        //exercise 4
+//        int num1 = 12;
+//        int num2 = 8;
+//        while (num2 != 0) {
+//            int num3 = num1 % num2;
+//            num1 = num2;
+//            num2 = num3;
+//        }
+//        System.out.println(num1);
+
+
+        //exercise 5
+//        for (int num1 = 100; num1 < 1000; num1++) {
+//            int num2 = num1 / 100;
+//            int num3 = num1 % 10;
+//            int num4 = num2 + num3;
+//
+//            if (num4 != 0 && num1 % num4 == 0) {
+//                System.out.println(num1);
+//            }
+//        }
+    }
+}
